91c261dc53ec5455388fb90961da7da02cd8287c
Ved Khandekar <vedk90@gmail.com>
2024-03-08 20:22:05 +0530

    Fixed formatting

--- test.py
+++ test.py
@@ -9,7 +9,13 @@ img = Image.open("Part5.jpg")
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel("gemini-pro-vision")
 response = model.generate_content(
-    ["Does the following material have any defect?", img, "Describe the defect in 50 words.", "Are you sure that the defect is not on the right side?"], stream=True
+    [
+        "Does the following material have any defect?",
+        img,
+        "Describe the defect in 50 words.",
+        "Are you sure that the defect is not on the right side?",
+    ],
+    stream=True,
 )
 
 print(response.prompt_feedback)
